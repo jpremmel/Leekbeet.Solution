6b3aa3d7c906cdb4c65128b91545873bf183b268
Jacqueline Remmel <jpremmel@gmail.com>
2019-10-12 14:26:01 -0700

    Add ability to preserve uppercase first character of all input words.

--- Leekbeet/Models/Translate.cs
+++ Leekbeet/Models/Translate.cs
@@ -27,103 +27,278 @@ namespace Leekbeet.Models
                 {
                     if (char.ToLower(inputArray[i][0]) == 'a')
                     {
-                        inputArray[i] = "asparagus";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Asparagus";
+                        }
+                        else
+                        {
+                            inputArray[i] = "asparagus";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'b')
                     {
-                        inputArray[i] = "beet";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Beet";
+                        }
+                        else
+                        {
+                            inputArray[i] = "beet";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'c')
                     {
-                        inputArray[i] = "carrot";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Carrot";
+                        }
+                        else
+                        {
+                            inputArray[i] = "carrot";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'd')
                     {
-                        inputArray[i] = "dill";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Dill";
+                        }
+                        else
+                        {
+                            inputArray[i] = "dill";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'e')
                     {
-                        inputArray[i] = "eggplant";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Eggplant";
+                        }
+                        else
+                        {
+                            inputArray[i] = "eggplant";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'f')
                     {
-                        inputArray[i] = "fennel";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Fennel";
+                        }
+                        else
+                        {
+                            inputArray[i] = "fennel";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'g')
                     {
-                        inputArray[i] = "garlic";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Garlic";
+                        }
+                        else
+                        {
+                            inputArray[i] = "garlic";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'h')
                     {
-                        inputArray[i] = "horseradish";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Horseradish";
+                        }
+                        else
+                        {
+                            inputArray[i] = "horseradish";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'j')
                     {
-                        inputArray[i] = "jicama";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Jicama";
+                        }
+                        else
+                        {
+                            inputArray[i] = "jicama";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'k')
                     {
-                        inputArray[i] = "kale";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Kale";
+                        }
+                        else
+                        {
+                            inputArray[i] = "kale";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'l')
                     {
-                        inputArray[i] = "leek";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Leek";
+                        }
+                        else
+                        {
+                            inputArray[i] = "leek";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'm')
                     {
-                        inputArray[i] = "mint";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Mint";
+                        }
+                        else
+                        {
+                            inputArray[i] = "mint";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'n')
                     {
-                        inputArray[i] = "nutmeg";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Nutmeg";
+                        }
+                        else
+                        {
+                            inputArray[i] = "nutmeg";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'o')
                     {
-                        inputArray[i] = "onion";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Onion";
+                        }
+                        else
+                        {
+                            inputArray[i] = "onion";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'p')
                     {
-                        inputArray[i] = "potato";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Potato";
+                        }
+                        else
+                        {
+                            inputArray[i] = "potato";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'q')
                     {
-                        inputArray[i] = "quinoa";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Quinoa";
+                        }
+                        else
+                        {
+                            inputArray[i] = "quinoa";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'r')
                     {
-                        inputArray[i] = "radish";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Radish";
+                        }
+                        else
+                        {
+                            inputArray[i] = "radish";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 's')
                     {
-                        inputArray[i] = "squash";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Squash";
+                        }
+                        else
+                        {
+                            inputArray[i] = "squash";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 't')
                     {
-                        inputArray[i] = "turnip";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Turnip";
+                        }
+                        else
+                        {
+                            inputArray[i] = "turnip";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'u')
                     {
-                        inputArray[i] = "unagi";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Unagi";
+                        }
+                        else
+                        {
+                            inputArray[i] = "unagi";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'v')
                     {
-                        inputArray[i] = "vanilla";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Vanilla";
+                        }
+                        else
+                        {
+                            inputArray[i] = "vanilla";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'w')
                     {
-                        inputArray[i] = "watercress";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Watercress";
+                        }
+                        else
+                        {
+                            inputArray[i] = "watercress";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'x')
                     {
-                        inputArray[i] = "xigua";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Xigua";
+                        }
+                        else
+                        {
+                            inputArray[i] = "xigua";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'y')
                     {
-                        inputArray[i] = "yam";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Yam";
+                        }
+                        else
+                        {
+                            inputArray[i] = "yam";
+                        }
                     }
                     else if (char.ToLower(inputArray[i][0]) == 'z')
                     {
-                        inputArray[i] = "zucchini";
+                        if (char.IsUpper(inputArray[i][0]))
+                        {
+                            inputArray[i] = "Zucchini";
+                        }
+                        else
+                        {
+                            inputArray[i] = "zucchini";
+                        }
                     }
                 }
                 if (punctuation)
